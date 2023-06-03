1b297b1d0c6046bbd5b7e36765777205c4b5b727
Davood Ahmad Bhat <bhatdavood21@gmail.com>
2023-06-03 18:58:46 +0530

    Reverse A String

---
+++ src/strings/ReverseAString.java
@@ -0,0 +1,32 @@
+package src.strings;
+
+import java.util.Scanner;
+
+public class ReverseAString {
+
+    /*
+    * Q:- Reverse A String in Java
+    * */
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String that needs to be reversed");
+        String s = sc.nextLine();
+        System.out.println("Actual String : " + s);
+        reverseString(s);
+
+    }
+
+    public static void reverseString(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--){
+            char c = str.charAt(i);
+            sb.append(c);
+        }
+
+        System.out.println("Reversed String : "+ sb.toString());
+    }
+
+}
